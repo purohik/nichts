bfe30c816c36f8e5ab2eb409febd775a1bc81c9e
purohik <purohitkrishnar@gmail.com>
2023-06-01 22:32:53 +0530

    Re-implementing CompleteScreen with clearer design.

--- app/src/main/java/com/purohik/nichts/MainActivity.kt
+++ app/src/main/java/com/purohik/nichts/MainActivity.kt
@@ -29,119 +29,119 @@ import androidx.compose.ui.unit.sp
 import com.purohik.nichts.ui.theme.NichtsTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            NichtsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Nichts("World")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      NichtsTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colorScheme.background
+        ) {
+          Nichts("World")
         }
+      }
     }
+  }
 }
 
 // TODO: remove this function since not used anywhere.
 @Composable
 fun Nichts(name: String, modifier: Modifier = Modifier) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = modifier
+  ) {
+    Column(
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = "Hello Android!",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Light,
-            )
-            Text(
-                text = name,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Light,
-            )
-            Text(
-                text = "My name is banana@",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Light,
-            )
-        }
+      Text(
+        text = "Hello Android!",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Light,
+      )
+      Text(
+        text = name,
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Light,
+      )
+      Text(
+        text = "My name is banana@",
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Light,
+      )
     }
+  }
 }
 
 @Composable
 fun FullScreen(word: String) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(50.dp)
-            .background(Color.Cyan)
-    ) {
-        Row {
-            Text(
-                text = "You should try..."
-            )
-        }
-        Row {
-            SuggestedWord(word = "krishna")
-        }
-        Row {
-            Text(
-                text = "Input the result..."
-            )
-        }
-        InputWord(word = "kekws")
+  Column(
+    horizontalAlignment = Alignment.CenterHorizontally,
+    modifier = Modifier
+        .fillMaxSize()
+        .padding(50.dp)
+        .background(Color.Cyan)
+  ) {
+    Row {
+      Text(
+        text = "You should try..."
+      )
     }
+    Row {
+      SuggestedWord(word = "krishna")
+    }
+    Row {
+      Text(
+        text = "Input the result..."
+      )
+    }
+    InputWord(word = "kekws")
+  }
 }
 
 @Composable
 fun SuggestedWord(word: String) {
-    Row {
-        for (letter in word) {
-            Text(text = letter.toString())
-        }
+  Row {
+    for (letter in word) {
+      Text(text = letter.toString())
     }
+  }
 }
 
 @Composable
 fun InputWord(word: String) {
-    Row {
-        for (letter in word) {
-            var letterState by remember {
-                mutableStateOf(LetterState.CORRECT)
-            }
-            Button(
-                onClick = {
-                    letterState = when (letterState) {
-                        LetterState.CORRECT -> LetterState.INCORRECT
-                        LetterState.INCORRECT -> LetterState.NOT_PRESENT
-                        LetterState.NOT_PRESENT -> LetterState.CORRECT
-                    }
-                },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = BUTTON_STATES[letterState]!!
-                )
-            ) {
-                Text(
-                    text = letter.toString(),
-                )
-            }
-        }
+  Row {
+    for (letter in word) {
+      var letterState by remember {
+        mutableStateOf(LetterState.CORRECT)
+      }
+      Button(
+        onClick = {
+          letterState = when (letterState) {
+            LetterState.CORRECT -> LetterState.INCORRECT
+            LetterState.INCORRECT -> LetterState.NOT_PRESENT
+            LetterState.NOT_PRESENT -> LetterState.CORRECT
+          }
+        },
+        colors = ButtonDefaults.buttonColors(
+          containerColor = BUTTON_STATES[letterState]!!
+        )
+      ) {
+        Text(
+          text = letter.toString(),
+        )
+      }
     }
+  }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun NichtsPreview() {
-    NichtsTheme {
+  NichtsTheme {
 //        FullScreen("banana")
-        CompleteScreen()
-    }
+    CompleteScreen()
+  }
 }
--- app/src/main/java/com/purohik/nichts/UiComponents.kt
+++ app/src/main/java/com/purohik/nichts/UiComponents.kt
@@ -3,28 +3,59 @@ package com.purohik.nichts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.purohik.nichts.LetterState.CORRECT
+import com.purohik.nichts.LetterState.INCORRECT
+import com.purohik.nichts.LetterState.NOT_PRESENT
 
 @Composable
 fun CompleteScreen() {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color(172, 125, 255, 255))
-    ) {
-        RenderWord("paris")
-    }
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier
+      .fillMaxSize()
+      .background(color = Color(172, 125, 255, 200))
+  ) {
+    RenderWord("paris")
+  }
 }
 
 @Composable
 fun RenderWord(word: String) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier.background(
+      color = Color(255, 50, 50, 100)
+    )
+  ) {
+    for (letter in word) {
+      var letterState by remember {
+        mutableStateOf(NOT_PRESENT)
+      }
+      Button(
+        onClick = {
+          letterState = when (letterState) {
+            CORRECT -> INCORRECT
+            INCORRECT -> NOT_PRESENT
+            NOT_PRESENT -> CORRECT
+          }
+        },
+        colors = ButtonDefaults.buttonColors(
+          containerColor = BUTTON_STATES[letterState]!!
+        )
+      ) {
+        Text(text = letter.toString())
+      }
     }
+  }
 }
